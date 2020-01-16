b8977668dcc5915971c6094de29be5de69710f47
Issouf Zabre <sheridanzabre@gmail.com>
2020-01-16 00:29:05 -0500

    signUp and login work

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,131 @@
+// Packages and Libraries
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Link } from 'react-router-dom';
+//Components
+import Home from './components/Home'
+import Dashboard from './components/Dashboard'
+import Login from './components/Login'
+import Signup from './components/Signup'
+import  ProtectedRoute from './components/ ProtectedRoute'
+import { login, getProfile, signUp } from './service/apiService'
+//CSS
 import './App.css';
+import authService from './service/authService';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isSignedIn: false,
+      user: {}
+    }
+  }
+
+
+  async componentDidMount() {
+    try{
+      const fetchUser = await getProfile()
+
+      this.setState(state => {
+        return {
+          isSignedIn: authService.isAuthenticated(),
+          user: fetchUser
+        }
+
+      })
+
+    } catch(e) {
+      throw e
+    }
+  }
+
+loginUser = async (credentials) => {
+  try {
+    const user = await login(credentials)
+
+    this.setState(state => {
+      return {
+        isSignedIn: true,
+        user: user
+      }
+    })
+  }
+  catch (e) {
+    throw e
+  }
+}
+
+
+signOutUser = () => {
+  authService.signOut()
+
+  this.setState(state => {
+    return {
+      isSignedIn: false,
+      user: {}
+    }
+  })
+}
+
+signUpUser = async (credentials) => {
+  
+  try {
+    await signUp(credentials)
+    const newUser = {email: credentials.email, password: credentials.password}
+    this.loginUser(newUser)
+  } catch (e) {
+    throw e
+  }
+}
+
+  render() {
+    const { isSignedIn , user} = this.state
+    
+    return (
+      <div className="App">
+  
+        <nav>
+          <div><Link to="/">Home</Link></div>
+          {
+            isSignedIn &&
+            <div><Link to="/dashboard">Dashboard</Link></div>
+
+          }
+          {
+            !isSignedIn ? (
+              <div><Link to="/login">Login</Link></div>
+            ) : (
+              <button onClick= {this.signOutUser}>Sign Out</button>
+            )
+          
+          }
+
+          {!isSignedIn ? (
+              <div><Link to="/signup">Sign up</Link></div>
+            ) : (
+              null
+            )
+          }
+        </nav>
+  
+        <main>
+          <Route exact path="/" component={Home} />
+          <ProtectedRoute path="/dashboard" 
+          user={user}
+          component={Dashboard} />
+          <Route 
+          path="/login"
+           render={(props) => <Login  {...props} handleLogin={this.loginUser} isSignedIn={isSignedIn}/>} />
+            <Route 
+            path='/signup' 
+            render = {(props) => <Signup {...props} handleSignUp={this.signUpUser} isSignedIn={isSignedIn} />}
+             />
+        </main>
+
+   
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/service/authService.js
@@ -0,0 +1,12 @@
+const authService = {
+    isAuthenticated: () => {
+        const token = localStorage.getItem('token')
+        return token ? true: false
+    },
+
+    signOut: () => {
+        localStorage.removeItem('token')
+    }
+}
+
+export default authService
